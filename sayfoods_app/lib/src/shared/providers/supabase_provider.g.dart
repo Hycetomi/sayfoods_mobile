f81b12665cd22f5b71be42a8a40d14ca9268f1de
Hycetomi <folasopefagbemi@gmail.com>
2026-03-05 15:54:49 +0100

    welcome screen

---
+++ sayfoods_app/lib/src/shared/providers/supabase_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'supabase_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$supabaseHash() => r'd2c76c8455442fcc1da2405d92d2e55b47bcaeb6';
+
+/// See also [supabase].
+@ProviderFor(supabase)
+final supabaseProvider = Provider<SupabaseClient>.internal(
+  supabase,
+  name: r'supabaseProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$supabaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SupabaseRef = ProviderRef<SupabaseClient>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
